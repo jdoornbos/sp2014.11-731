311f9c2999712391086ca3601b1988ff69aae943
Jeremy Doornbos <jeremy.doornbos@gmail.com>
2014-02-18 23:41:48 -0500

    added symmetrization

--- hw1/m1align.py
+++ hw1/m1align.py
@@ -10,10 +10,13 @@ from collections import defaultdict
 
 if __name__ == '__main__':
 
-  def getbest(w1, other_sent, p_dist):
+  def getbest(w1, other_sent, p_dist, exactMatches):
     maxprob = 0
     aligned = len(other_sent) - 1
     for w2 in other_sent:
+      if (w1 == w2) and (w1 in exactMatches):
+        aligned = other_sent.index(w2)
+        return aligned
       if p_dist[(w2,w1)] > maxprob:
         maxprob = p_dist[(w2,w1)]
         aligned = other_sent.index(w2)
@@ -21,22 +24,37 @@ if __name__ == '__main__':
 
   def train(bisents):
     for (source, target) in bisents:
-      source.append("")
-
+      source.append("$null$")
+ 
     # Create word lists
     print "*** Word lists"
     source_words = set()
     target_words = set()
-    
+    matchCounts = defaultdict(lambda: [0,0])
     iteration = 1
     for (source, target) in bisents:
-      if iteration % (size / 10) == 0:
+      s_set = set(source)
+      t_set = set(target)
+      source_words.update(s_set)
+      target_words.update(t_set)
+      if iteration % (trainsize / 10) == 0:
         print "sentence:", iteration
-      for word in source:
-        source_words.add(word)
-      for word in target:
-        target_words.add(word)
+      for t in target:
+        if t in s_set:
+          matchCounts[t][0] += 1
+          matchCounts[t][1] += 1
+        else:
+          matchCounts[t][1] += 1
+      for s in source:
+        if s not in t_set:
+          matchCounts[s][1] += 1
       iteration += 1
+      
+    exactMatches = set()
+    for word in matchCounts:
+      matchRate = float(matchCounts[word][0])/float(matchCounts[word][1])*100
+      if matchRate >= 1 and (matchRate + len(word)) >= 15:
+        exactMatches.add(word)
     
     source_len = len(source_words)
     target_len = len(target_words)
@@ -47,14 +65,14 @@ if __name__ == '__main__':
     total = defaultdict(float)
     subtotal = defaultdict(float)
     
-    print source_len - 1, "German words"
-    print target_len, "English words"
+    print source_len - 1, "Source words"
+    print target_len, "Target words"
     
     # Create co-occurrance counts
     print "*** Co-occurrance counts"
     sentnum = 0
-    for (source, target) in bitext:
-      if sentnum % (size / 10) == 0:
+    for (source, target) in bisents:
+      if sentnum % (trainsize / 10) == 0:
           print "sentence:", sentnum
       sentnum += 1
       for s in source:
@@ -77,7 +95,7 @@ if __name__ == '__main__':
       # collect counts
       sentnum = 0
       for (source, target) in bisents:
-        if sentnum % (size / 10) == 0:
+        if sentnum % (trainsize / 10) == 0:
           print "   sentence:", sentnum
         sentnum += 1
         for s_word in source:
@@ -88,58 +106,100 @@ if __name__ == '__main__':
       # estimate probabilities
       for (s_word, t_word) in p:
         p[(s_word, t_word)] = count[(s_word, t_word)] / total[t_word]
-        
-    return p
+    
+    print    
+    return p, exactMatches
   
   
   
   
-  numtoprint = 10
+  numtoprint = 2
   goldData = 'data//dev.align'
   gold = [aligns.strip() for aligns in open(goldData)][:numtoprint]
   
-  size = 20000
+  trainsize = 10000
+  testsize = 50000
   data = 'data//dev-test-train.de-en'
 #   data = 'data//tiny.de-en'
-  bitext = [[sentence.strip().lower().split() for sentence in pair.split(' ||| ')] for pair in open(data)][:size]
-  bisents = bitext
+  bitext =  [[sentence.strip().lower().split() for sentence in pair.split(' ||| ')] for pair in open(data)][:testsize]
+  bitext_st = [[sentence.strip().lower().split() for sentence in pair.split(' ||| ')] for pair in open(data)][:trainsize]
+  bitext_ts = [[sentence.strip().lower().split() for sentence in pair.split(' ||| ')] for pair in open(data)][:trainsize]
   
-  # Reverse the SOURCE and TARGET
-#   for sent in bisents:
-#     sent.reverse()
+  for sent in bitext_ts:
+    sent.reverse()
     
-  prob_dist = train(bisents)
+  (prob_dist, exacts)  = train(bitext_st)
+
+  (prob_dist2, exacts)  = train(bitext_ts)
   
-  # print to console
-  for (n, (source, target)) in enumerate(bisents):
-    if n >= numtoprint:
-      break
-    print " ".join(source)
-    print gold[n]
-    print " ".join(target)
+
+  output = open("output.txt", 'w')
+  
+  
+  print "*** Write alignments"
+  sentnum = 0
+  for (source, target) in bitext:
+    if sentnum % (testsize / 10) == 0:
+      print "   sentence:", sentnum
+    sentnum += 1
+        
+#     if n >= numtoprint:
+#       break
+#     print " ".join(source)
+#     print gold[n]
+#     print " ".join(target)
+    
+    aligns1 = set()
     for word in target:
-      best_align = getbest(word, source, prob_dist)
-      if best_align < len(source) - 1:
+      best_align = getbest(word, source, prob_dist, exacts)
+      if best_align < len(source):
         sentOutput = str(best_align) + '-' + str(target.index(word))
+        aligns1.add((best_align, target.index(word)))
       else:
         sentOutput = str(best_align) + '-$'
-      print sentOutput,
+#       print sentOutput,
+#     print
+#     print aligns1
+    
+    aligns2 = set()
+    for word in source:
+      best_align = getbest(word, target, prob_dist2, exacts)
+      if best_align < len(target):
+        sentOutput = str(source.index(word)) + '-' +  str(best_align)
+        aligns2.add((source.index(word), best_align))
+      else:
+        sentOutput = '$-' + str(best_align)
+#       print sentOutput,
+#     print
+#     print aligns2
+    
+#     print
+    sure_aligns = set.intersection(aligns1, aligns2)
+#     print sure_aligns
+#     poss_aligns = set.union(aligns1, aligns2) - sure_aligns
+#     print poss_aligns
+    
+    sentOutput = ''
+    for (s, t) in sure_aligns:
+      sentOutput += str(s) + '-' + str(t) + ' '
+#     for (s, t) in poss_aligns:
+#       sentOutput += str(s) + '-' + str(t) + ' '
       
-    print
-    for word in target:
-      print source[getbest(word, source)] + '-' + word,
-    print
-    print
-  
-  # print to output file
-  output = open("output.txt", 'w')
-  for (source, target) in bisents:
-    sentOutput = ""
-    for word in target:
-      best_align = getbest(word, source, prob_dist)
-      if best_align < len(source) - 1:
-        # non-reversed
-        sentOutput += str(best_align) + '-' + str(target.index(word)) + ' '
-        # reversed
-#         sentOutput += str(target.index(word)) + '-' + str(getbest(word, source)) + ' ' 
+#     print sentOutput
     output.write(sentOutput.strip() + "\n")
+#     for word in target:
+#       print source[getbest(word, source)] + '-' + word,
+#     print
+#     print
+  print 'Done!'
+  # print to output file
+#   for (source, target) in bitext:
+#     sentOutput = ""
+#     for word in target:
+#       best_align = getbest(word, source, prob_dist, exacts)
+#       if best_align < len(source) - 1:
+#         # non-reversed
+#         sentOutput += str(best_align) + '-' + str(target.index(word)) + ' '
+#         # reversed
+# #         sentOutput += str(target.index(word)) + '-' + str(getbest(word, source)) + ' ' 
+#     output.write(sentOutput.strip() + "\n")
